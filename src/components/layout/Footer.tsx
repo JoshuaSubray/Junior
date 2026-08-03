dbeb6cdcb615e06ceb731410bd97d8bf8ca33af7
JoshuaSubray <subrayjoshua@gmail.com>
2026-08-03 18:02:24 -0400

    Added icon and source code terms in the About page.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer" id="footer">
-      <p>&copy; {new Date().getFullYear()} Junior Grade Calculator. All rights reserved.</p>
+      {<p>&copy; {new Date().getFullYear()} Junior: Grade Calculator. All rights reserved.</p> }
     </footer>
   )
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,6 +12,11 @@ export default function About() {
         <h3 className="about-sub-heading">How To Use</h3>
         <p>TBA.</p>
 
+        <h3 className="about-sub-heading">Source Code</h3>
+        <p>
+          Our code is completely source-available on GitHub, however protected under the <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-ND 4.0</a> license. Please contact us if you wish to use our code for any reason.
+        </p>
+
         <h3 className="about-sub-heading">Donate</h3>
         <p>
           Want to help keep Junior free, accessible, and source-available? Consider donating to support the developers and their future endeavors! Every contribution is greatly appreciated.
