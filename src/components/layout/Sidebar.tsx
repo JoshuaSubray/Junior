20570b214871178e9012f890c5d8102345aeb7f2
JoshuaSubray <subrayjoshua@gmail.com>
2026-08-07 17:34:19 -0400

    Changed the semester write symbol from '>' to 'W' to prevent compilation errors.

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -79,7 +79,7 @@ export default function Sidebar() {
                       }}
                       title="Rename"
                     >
-                      >
+                      W
                     </button>
                     <span 
                       style={{ 
